04c6f821d5a4b7b7baf159cfaa77813173a44b9e
Innocent866 <igoldima@gmail.com>
2024-01-13 13:05:48 -0800

    goal-on

--- src/pages/AllUser.jsx
+++ src/pages/AllUser.jsx
@@ -5,16 +5,13 @@ import Spinner from 'react-bootstrap/Spinner'
 
 const AllUser = () => {
   const [data, setData] = useState([])
-  // const [noUse, setNoUse] = useState('')
   const [isLoading, setIsLoading] = useState(false)
 
   const getData = async ()=>{
     try{
       setIsLoading(true);
       const fetcher = await axios.get('https://goal-z8dh.onrender.com/api/user');
-      console.log(fetcher.data.users);
-      console.log(fetcher.data.msg);
-      // setNoUse(fetcher.data.msg)
+      console.log(fetcher.data);
       setData(fetcher.data.users)
     }catch(error){
       console.log(error);
@@ -27,31 +24,30 @@ const AllUser = () => {
     getData()
   },[])
   return (
-    <main>
+    <main className='container pt-4'>
       <h2>All Users</h2>
       {isLoading && <Spinner amination= 'border'/>}
       <div className="container pt-5 row justify-content-center align-items-center gap-4">
-        {data && data.length < 1 ? 
-        ( 
-         <h1 className="text-danger">No users yet... Create one</h1> 
+        
+        {data && data.length < 1 ? (
+          <h1 className='text-danger'>no user found... Create One</h1>
         ) : (
-          data.map((datum)=>{
-           const {_id,name,email,gender,profession}=datum
-          
-            return(
-              <div className="card col-lg-5 p-3 shadow-sm" key={_id}>
-                <Link className='text-decoration-none' to={`/SingleUser/${_id}`}>
-                  <h2>{name}</h2>
-                  <p>{email}</p>
-                  <p>{gender}</p>
-                  <p>{profession}</p>
-                  <span className='text-danger'>Click to see more</span>
-                </Link>
-              </div>
-            )
-          })
-        )
-        }
+            data.map((datum)=>{
+             const {_id,name,email,gender,profession} = datum
+            
+              return(
+                <div className="card col-lg-5 p-3 shadow-sm" key={_id}>
+                  <Link className='text-decoration-none' to={`/SingleUser/${_id}`}>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                    <p>{gender}</p>
+                    <p>{profession}</p>
+                    <span className='text-danger'>Click to see more</span>
+                  </Link>
+                </div>
+              )
+            })
+        )}
       </div>
     </main>
   )
